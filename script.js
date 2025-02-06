14ecbe2df3c3656db43d2731df1a61f5002ab20b
thaliasilva27 <160746094+thaliasilva27@users.noreply.github.com>
2025-02-06 10:39:40 -0300

    fbggftgh

---
+++ script.js
@@ -0,0 +1,26 @@
+ocument.addEventListener("DOMContentLoaded", function() {
+    const textElement = document.querySelector(".text");
+    const cursorElement = document.querySelector(".cursor");
+    const text = textElement.textContent;
+    textElement.textContent = "";
+    
+    let index = 0;
+
+    function type() {
+        if (index < text.length) {
+            textElement.textContent += text.charAt(index);
+            index++;
+            setTimeout(type, 100);
+        } else {
+            setTimeout(reset, 2000); // Espera 2 segundos antes de reiniciar
+        }
+    }
+
+    function reset() {
+        textElement.textContent = "";
+        index = 0;
+        type();
+    }
+
+    type();
+});
